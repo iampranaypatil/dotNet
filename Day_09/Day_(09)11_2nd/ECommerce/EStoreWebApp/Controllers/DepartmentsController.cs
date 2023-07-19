aa24c6e78ec4cf4a5372e75a76aa2c2a96a9c515
Pranay Patil <pranaypatil8055@gmail.com>
2023-07-19 23:23:36 +0530

    day_09

---
+++ Day_09/Day_(09)11_2nd/ECommerce/EStoreWebApp/Controllers/DepartmentsController.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using EStoreWebApp.Models;
+
+using BOL;
+using BLL;
+
+namespace EStoreWebApp.Controllers;
+
+public class DepartmentsController : Controller
+{
+    private readonly ILogger<DepartmentsController> _logger;
+
+    public DepartmentsController(ILogger<DepartmentsController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        HRManager mgr=new HRManager();
+        List<Department> departments=mgr.GetAllDepartments();
+        // it will calling views --> Departments ---> index.cshtml 
+        this.ViewData["departments"]=departments;
+        return View();
+    }
+
+     
+}
---
+++ Day_09/Day_(09)11_2nd/ECommerce/TestApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Testing;
+using HR;
+
+
+Department department = new Department();
+Console.WriteLine("Enter Employee Id: ");
+department.Id = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Employee Name: ");
+department.Name = Console.ReadLine();
+
+Console.WriteLine("Enter Employee location: ");
+department.Location = Console.ReadLine();
+
+bool insert = DbTestManager.Insert(department);
+if (insert)
+    Console.WriteLine("Data Inserted");
+else
+    Console.WriteLine("Data not Inserted");
+
+List<Department> departments=DbTestManager.GetAllDepartments();
+foreach ( Department dept in departments){
+    Console.WriteLine(dept.Name + "  " + dept.Location);
+}
+
+//Database Operations unit Testing
+
