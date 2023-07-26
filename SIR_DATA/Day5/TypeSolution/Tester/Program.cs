b007848bc0fac484f736f706a00a877d46c0c79b
Pranay Patil <pranaypatil8055@gmail.com>
2023-07-26 14:20:16 +0530

    Dotnet(JWT)+Sdm

---
+++ SIR_DATA/Day5/TypeSolution/Tester/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Reflection;
+string path=@"C:\Users\rr\Desktop\DOT_NET\Sir_Dotnet_DayWise_data\dotnet\Day5\TypeSolution\Catalog\bin\Debug\net7.0\Catalog.dll";
+Assembly asm=Assembly.LoadFile(path);
+string name=asm.FullName;
+Console.WriteLine(name);
+
+Type[] types=asm.GetTypes();
+for( int i=0;i<types.Count();i++){
+
+    string typeName=types[i].Name;
+    Console.WriteLine(typeName);
+    MethodInfo [] mi=types[i].GetMethods();
+    foreach( MethodInfo m in mi){
+        string methodName=m.Name;
+        Console.WriteLine(methodName);
+    }
+
+}
+
+
+
